f8ec5037b52f628ea29bae6d7aa5a588e0096200
Paulo Roberto <pauloreciere@gmail.com>
2022-02-10 17:09:31 -0300

    Adding Fake Delay to look like a backend request

--- src/services/product.ts
+++ src/services/product.ts
@@ -1,11 +1,13 @@
 import products from '../assets/pizzas.json';
 
+const fakeDelay = Math.floor(Math.random() * (800 - 50)) + 50;
+
 const getWaitPromise = (time: number) =>
 	new Promise((resolve) => setTimeout(resolve, time));
 
 export const getProducts = () => {
 	const fnPromise = Promise.resolve(() => products);
-	const waitPromise = getWaitPromise(0);
+	const waitPromise = getWaitPromise(fakeDelay);
 
 	return Promise.all([fnPromise, waitPromise]).then((values) => values[0]);
 };
